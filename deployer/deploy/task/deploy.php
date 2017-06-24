4168d1b978be2218d1fc626178af828ed851909b
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-24 23:58:00 +0200

    Refactor deploy task.

--- deployer/deploy/task/deploy.php
+++ deployer/deploy/task/deploy.php
@@ -3,16 +3,64 @@
 namespace Deployer;
 
 task('deploy', [
+    // Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-lock
+    'deploy:check_lock',
+
+    // Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-composer-install
+    'deploy:check_composer_install',
+
+    // Standard deployer deploy:prepare
     'deploy:prepare',
+
+    // Standard deployer deploy:lock
+    'deploy:lock',
+
+    // Standard deployer deploy:release
     'deploy:release',
+
+    // Standard deployer deploy:update_code
     'deploy:update_code',
+
+    // Standard deployer deploy:shared
     'deploy:shared',
+
+    // Standard deployer deploy:writable
     'deploy:writable',
+
+    // Standard deployer deploy:vendors
     'deploy:vendors',
+
+    // Detect WP version and get fresh code from WordPress git repo
     'deploy:wp:core',
+
+    // Standard deployer deploy:copy_dirs. Copy plugins from previous release of Wordpress
+    'deploy:copy_dirs',
+
+    // Standard deployer deploy:clear_paths
     'deploy:clear_paths',
+
+    // Clear php cli cache.
+    // Read more on https://github.com/sourcebroker/deployer-extended#php-clear-cache-cli
     'php:clear_cache_cli',
+
+    // Start buffering http requests. No frontend access possbile from now.
+    // Read more on https://github.com/sourcebroker/deployer-extended#buffer-start
+    'buffer:start',
+
+    // Standard deployer symlink (symlink release/x/ to current/)
     'deploy:symlink',
+
+    // Clear frontend http cache.
+    // Read more on https://github.com/sourcebroker/deployer-extended#php-clear-cache-http
     'php:clear_cache_http',
+
+    // Frontend access possbile again from now
+    // Read more on https://github.com/sourcebroker/deployer-extended#buffer-stop
+    'buffer:stop',
+
+    // Standard deployer deploy:unlock
+    'deploy:unlock',
+
+    // Standard deployer cleanup.
     'cleanup',
 ])->desc('Deploy your Wordpress');
