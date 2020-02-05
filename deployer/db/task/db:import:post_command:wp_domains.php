43561b7adfef2a22ae60583670281f50e2355a6d
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2020-02-05 18:23:05 +0100

    [TASK] Deployer 6 compatibility

--- deployer/db/task/db:import:post_command:wp_domains.php
+++ deployer/db/task/db:import:post_command:wp_domains.php
@@ -2,32 +2,29 @@
 
 namespace Deployer;
 
+use SourceBroker\DeployerExtendedDatabase\Utility\ConsoleUtility;
+use SourceBroker\DeployerInstance\Configuration;
+
 /**
  * Replace domains in current instance.
  */
 task('db:import:post_command:wp_domains', function () {
-    if (input()->getOption('dumpcode')) {
-        $dumpCode = input()->getOption('dumpcode');
-    } else {
-        throw new \Exception('No dumpcode set. [Error code: 1498321469492]');
-    }
-    $dumpsForDumpCode = glob(get('db_storage_path_current') . '/' . '*dumpcode:' . $dumpCode . '*');
+
+    $dumpCode = (new ConsoleUtility())->getOption('dumpcode', true);
+    $dumpsForDumpCode = glob(get('db_storage_path_current') . '/' . '*dumpcode=' . $dumpCode . '*');
     if (empty($dumpsForDumpCode)) {
         throw new \Exception('Can not find dumps for dumpcode: ' . $dumpCode . '. [Error code: 1498321476975]');
     }
     $sourceInstanceName = null;
-    preg_match('/\#server:(.*)\#/U', reset($dumpsForDumpCode), $match);
+    preg_match('/\#server=(.*)\#/U', reset($dumpsForDumpCode), $match);
     if (isset($match[1])) {
         $sourceInstanceName = $match[1];
     } else {
         throw new \Exception('Can not determine source instance based on dump filename. [Error code: 1498321481427]');
     }
     $currentInstancePublicUrls = get('public_urls');
-    if (isset(Deployer::get()->environments[$sourceInstanceName])) {
-        $sourceInstance = Deployer::get()->environments[$sourceInstanceName];
-    } else {
-        throw new \Exception('Can not find instance with name: ' . $sourceInstanceName . ' [Error code: 1498321487045]');
-    }
+
+    $sourceInstance = Configuration::getHost($sourceInstanceName);
     $sourceInstancePublicUrls = $sourceInstance->get('public_urls');
     if (count(get('public_urls')) === count($sourceInstance->get('public_urls'))) {
         $publicUrlsPairs = array_combine($sourceInstancePublicUrls, $currentInstancePublicUrls);
